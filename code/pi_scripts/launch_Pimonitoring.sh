23bfa3adf552f60c061699101a7548d5ce1cd087
br-mat <76951337+br-mat@users.noreply.github.com>
2022-03-16 15:33:34 +0100

    created .sh launcher scripts

---
+++ code/pi_scripts/launch_Pimonitoring.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# launch_Pimonitoring.sh
+cd /home/pi/py_scripts
+python3 monitorPi.py -db=pidb -sn=test3
---
+++ code/pi_scripts/mqttbridge_launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# mqttbridge_launcher.sh
+
+sleep 60s
+
+cd /home/pi/py_scripts
+python3 MQTTInfluxDBBridge3.py
