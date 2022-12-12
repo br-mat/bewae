b5602de03747eb5c612471ab418d34b71ce747ba
br-mat <matthiasbraun@gmx.at>
2022-12-13 00:41:42 +0100

    Create connection.h
    sepperated wifi connection, and put all into a seperate file

---
+++ code/esp_32ard_bewae/src/connection.h
@@ -0,0 +1,29 @@
+#ifndef __CONNECTION_H__
+#define __CONNECTION_H__
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Wifi Constants
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // WiFi
+    #ifndef ssid
+    #define ssid "************"         // Your personal network SSID
+    #endif
+
+    #ifndef wifi_password
+    #define wifi_password "************" // Your personal network password
+    #endif
+
+    // MQTT
+    #ifndef mqtt_server
+    #define mqtt_server "************"  // IP of the MQTT broker
+    #endif
+
+    #ifndef mqtt_username
+    #define mqtt_username "************" // MQTT username
+    #endif
+
+    #ifndef mqtt_password
+    #define mqtt_password "************" // MQTT password
+    #endif
+
+#endif
