c230bfe5610ff7b2a8712876e816452e165fe1fc
br-mat <matthiasbraun@gmx.at>
2023-07-18 18:14:30 +0200

    updated sensor handlers
    added more sensor classes for individual sensors,
    changed the constructor to let them initialize directly from json objects

--- code/esp_32ard_bewae/src/SensorController.cpp
+++ code/esp_32ard_bewae/src/SensorController.cpp
@@ -64,11 +64,11 @@ MeasuringController::~MeasuringController() {}
 
 // Override virtual measure function from base class
 float MeasuringController::measure() {
-  if (measurementFunction) {
-      return measurementFunction(); // execute measurement function
-  } else {
-      return 0.0f; // return 0 if no function passed
-  }
+    if (measurementFunction) {
+        return measurementFunction(); // execute measurement function
+    } else {
+        return 0.0f; // return 0 if no function passed
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,36 +76,238 @@ float MeasuringController::measure() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Total Constructor
-VpinController::VpinController(const String& name, int vpin, int low_limit, int high_limit, float factor)
-    : BasicSensor(name), virtualPin(vpin), low_limit(low_limit), high_limit(high_limit), factor(factor) {}
+VpinController::VpinController(const String& name, int vpin, int low_limit, int high_limit, int additive, float factor)
+    : BasicSensor(name), virtualPin(vpin), low_limit(low_limit), high_limit(high_limit), additive(additive), factor(factor) {}
 
 // Short Constructor
-VpinController::VpinController(const String& name, int vpin) : VpinController(name, vpin, 0, 0, 1) {}
+VpinController::VpinController(const String& name, int vpin) : VpinController(name, vpin, 0, 0, 0, 1.0) {}
+
+// JSON Obj Constructor
+VpinController::VpinController(const String& name, const JsonObject& sensorConfig) : BasicSensor(name) { 
+    // validate JSON: Check for the presence of the additive and factor properties
+    if (sensorConfig.containsKey("add")) {
+        additive = sensorConfig["add"].as<int>();
+    } else {
+        additive = 0.0;
+    }
+    if (sensorConfig.containsKey("fac")) {
+        factor = sensorConfig["fac"].as<float>();
+    } else {
+        factor = 1.0;
+    }
+    if (sensorConfig.containsKey("hlim")) {
+        high_limit = sensorConfig["hlim"];
+    } else {
+        high_limit = 0;
+    }
+    if (sensorConfig.containsKey("llim")) {
+        low_limit = sensorConfig["llim"];
+    } else {
+        low_limit = 0;
+    }
+    if (sensorConfig.containsKey("vpin")) {
+        virtualPin = sensorConfig["vpin"].as<uint16_t>();
+    } else {
+        virtualPin = 0;
+    }
+}
 
 // Override virtual measure function from base class
 float VpinController::measure() {
-  int result = 0;
-  Helper::controll_mux(this->virtualPin, sig_mux_1, en_mux_1, "read", &result); // use virtualPin on MUX and read its value
+    // measurement performed either default or relative (in %)
+    int result = 0;
+    Helper::controll_mux(this->virtualPin, sig_mux_1, en_mux_1, "read", &result); // use virtualPin on MUX and read its value
 
-  // return result
-  return this->factor * (float)result;
+    // check if rel measurement is needed
+    if ((high_limit != 0) && (low_limit != 0)) {
+        float temp = (float)result * measurement_LSB5V * 1000;
+        #ifdef DEBUG
+        Serial.print(F("raw read:")); Serial.println(result);
+        Serial.print(F("raw read int:")); Serial.println((int)temp);
+        #endif
+        result = temp + 0.5; // cast remporary float to int and round
+        result = constrain(result, this->low_limit, this->high_limit); //x within borders else x = border value; (example 1221 wet; 3176 dry [in mV])
+                                                        //avoid using other functions inside the brackets of constrain
+        result = map(result, this->low_limit, this->high_limit, 1000, 0) / 10;
+    }
+
+    // return result
+    return this->factor * (float)result + this->additive;
 }
 
-// returns relative measurement in %
-float VpinController::measureRel() {
-  int value = 0;
-  Helper::controll_mux(this->virtualPin, sig_mux_1, en_mux_1, "read", &value); // use virtualPin on MUX and read its value
-  float temp = (float)value * measurement_LSB5V * 1000;
-  #ifdef DEBUG
-  Serial.print(F("raw read:")); Serial.println(value);
-  Serial.print(F("raw read int:")); Serial.println((int)temp);
-  #endif
-  value = temp;
-  value = constrain(value, this->low_limit, this->high_limit); //x within borders else x = border value; (example 1221 wet; 3176 dry [in mV])
-                                                //avoid using other functions inside the brackets of constrain
-  value = map(value, this->low_limit, this->high_limit, 1000, 0) / 10;
-  
-  // return result
-  return this->factor * (float)value;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  PinController
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Total Constructor
+PinController::PinController(const String& name, int pin, int low_limit, int high_limit, int additive, float factor)
+    : BasicSensor(name), pin(pin), low_limit(low_limit), high_limit(high_limit), additive(additive), factor(factor) {}
+
+// Short Constructor
+PinController::PinController(const String& name, int pin) : PinController(name, pin, 0, 0, 0, 1.0) {}
+
+// JSON Obj Constructor
+PinController::PinController(const String& name, const JsonObject& sensorConfig) : BasicSensor(name) { 
+    // validate JSON: Check for the presence of the additive and factor properties
+    if (sensorConfig.containsKey("add")) {
+        additive = sensorConfig["add"].as<int>();
+    } else {
+        additive = 0.0;
+    }
+    if (sensorConfig.containsKey("fac")) {
+        factor = sensorConfig["fac"].as<float>();
+    } else {
+        factor = 1.0;
+    }
+    if (sensorConfig.containsKey("hlim")) {
+        high_limit = sensorConfig["hlim"];
+    } else {
+        high_limit = 0;
+    }
+    if (sensorConfig.containsKey("llim")) {
+        low_limit = sensorConfig["llim"];
+    } else {
+        low_limit = 0;
+    }
+    if (sensorConfig.containsKey("pin")) {
+        pin = sensorConfig["pin"].as<uint16_t>();
+    } else {
+        pin = 0.0;
+    }
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Override the measure function to read from an Arduino pin
+float PinController::measure() {
+    int num = 15;
+    int result = 0;
+    // throw away
+    for(int j = 0;j<5;j++){
+        result = analogRead(pin); // Read the value from the specified pin
+        delayMicroseconds(100);
+    }
+    // take mean
+    result = 0;
+    float mean = 0;
+    for(int j = 0;j<num;j++){
+        mean += analogRead(pin); // Read the value from the specified pin
+        delayMicroseconds(100);
+    }
+    result = (mean/num)+0.5;
+    return this->factor * result + this->additive;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  SoilTempSensor
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Total Constructor
+SoilTempSensor::SoilTempSensor(const String& name, int addr, int additive, float factor)
+    : BasicSensor(name), addr(addr), additive(additive), factor(factor), sensors(&oneWire) {}
+
+// Short Constructor
+SoilTempSensor::SoilTempSensor(const String& name, int addr) : SoilTempSensor(name, addr, 0, 1.0) {}
+
+// JSON Obj Constructor
+SoilTempSensor::SoilTempSensor(const String& name, const JsonObject& sensorConfig) : BasicSensor(name) { 
+    // validate JSON: Check for the presence of the additive and factor properties
+    if (sensorConfig.containsKey("add")) {
+        additive = sensorConfig["add"].as<int>();
+    } else {
+        additive = 0.0;
+    }
+    if (sensorConfig.containsKey("fac")) {
+        factor = sensorConfig["fac"].as<float>();
+    } else {
+        factor = 1.0;
+    }
+    if (sensorConfig.containsKey("addr")) {
+        addr = sensorConfig["addr"].as<uint16_t>();
+    } else {
+        addr = 0.0;
+    }
+
+    oneWire = OneWire(addr);
+    sensors = DallasTemperature(&oneWire);
+}
+
+// Override the measure function to read from an Arduino pin
+float SoilTempSensor::measure() {
+    sensors.begin();  // Initialize the DS18B20 sensor
+    sensors.requestTemperatures();  // Request temperature conversion
+
+    // Read temperature from DS18B20 sensor
+    float temperatureC = sensors.getTempCByIndex(0);
+
+    // Apply additive and factor
+    temperatureC = temperatureC + additive;
+    temperatureC = temperatureC * factor;
+
+    // Return the measured and processed temperature value
+    return temperatureC;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  bmeSensor
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Total Constructor
+bmeSensor::bmeSensor(const String& name, int addr, int mode)
+    : BasicSensor(name), addr(addr) {}
+
+// JSON Obj Constructor
+bmeSensor::bmeSensor(const String& name, const JsonObject& sensorConfig) : BasicSensor(name) { 
+    // validate JSON: Check for the presence of the additive and factor properties
+    if (sensorConfig.containsKey("addr")) {
+        addr = sensorConfig["addr"].as<int>();
+    } else {
+        addr = BME280_I2C_ADDRESS; // use default if not specified
+    }
+    // mode: 0 = temp, 1 = humidity, 2 = pressure
+    if (sensorConfig.containsKey("mode")) {
+        mode = sensorConfig["mode"].as<int>();
+    } else {
+        mode = 0; // use default if not specified
+    }
+}
+
+// Override the measure function to read from an Arduino pin
+float bmeSensor::measure() {
+    // Initialize the BME280 sensor
+    if (!bme.begin(addr)) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        return 0;
+    }
+
+    float result;
+    switch (mode)
+    {
+    case 0:
+        result = bme.readTemperature();
+        break;
+    case 1:
+        result = bme.readHumidity();
+        break;
+    case 2:
+        result = bme.readPressure();
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+// TODO:
+// - implement bme280
+// - add unified publishing method to base class, use default on most of them and on bme custom one
+// - build test enironment for only this class + a config file
+// - add seperate config file
+// - remove dht
+
+// TODO: implement new json constructor to all subclasses
+//       implement new classes for normal pin not vpin etc.// TODO: implement new json constructor to all subclasses
+//       implement new classes for normal pin not vpin etc.
+
+// Hae
+// - ds12b2
+// - analog moist 5v
--- code/esp_32ard_bewae/src/SensorController.h
+++ code/esp_32ard_bewae/src/SensorController.h
@@ -32,6 +32,21 @@
 #include <Arduino.h>
 #include <Helper.h>
 #include <functional>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sensor Setup
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+
+// Setup a oneWire instance to communicate with any OneWire devices
+//OneWire WireBus(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+//DallasTemperature sensors(&WireBus);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // BasicSensor
@@ -39,7 +54,7 @@
 
 // This is the Basic class holding sensor name, a status variable and a dummy measure function
 class BasicSensor {
-public:
+  public:
     // Basic constructor that takes the name of the sensor as an argument
     BasicSensor(const String& name);
 
@@ -56,7 +71,7 @@ public:
     bool getStatus() const;
     void setStatus(bool status);
 
-protected:
+  private:
     String sensorName; // Name of the sensor
     bool status;       // Status of the sensor (true = on, false = off)
 };
@@ -67,7 +82,7 @@ protected:
 
 // Thias class should take a custom measurement function to read a sensor
 class MeasuringController : public BasicSensor {
-public:
+  public:
     // Constructor that takes the name of the sensor and a std::function as arguments.
     // The measurement function is optional and defaults to nullptr if not provided.
     MeasuringController(const String& name, std::function<float()> measurementFunction = nullptr);
@@ -78,11 +93,11 @@ public:
     // Override virtual measure function from base class to return the result of calling the measurementFunction.
     float measure() override;
 
-private:
+  private:
     std::function<float()> measurementFunction; // std::function that returns a float and takes no arguments.
 
-    using BasicSensor::sensorName;
-    using BasicSensor::status;
+    //using BasicSensor::sensorName;
+    //using BasicSensor::status;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,26 +106,106 @@ private:
 
 // This class should be read analog values and give back either raw reading or relative reading within boarders
 class VpinController : public BasicSensor {
-public:
-  // Total Constructor
-  VpinController(const String& name, int vpin, int low_limit, int high_limit, float factor);
+  public:
+    // Total Constructor
+    VpinController(const String& name, int vpin, int low_limit, int high_limit, int additive, float factor);
 
-  // Short Constructor
-  VpinController(const String& name, int vpin);
+    // Short Constructor
+    VpinController(const String& name, int vpin);
 
-  // Override virtual measure function from base class to read and return the value from the specified virtual pin.
-  float measure() override;
-  float measureRel();
+    // JSON Obj Constructor
+    // pass name (key) and config (value)
+    VpinController(const String& name, const JsonObject& sensorConfig);
 
-private:
-  int virtualPin; // Virtual pin number associated with this controller.
-  int low_limit; // Lower limit value for measurement
-  int high_limit; // Upper limit value for measurement
-  float factor; // Factor to multiply measurement values by
+    // Override virtual measure function from base class to read and return the value from the specified virtual pin.
+    float measure() override; // measurement performed either default or relative (in %)
 
-  using BasicSensor::sensorName;
-  using BasicSensor::status;
+  private:
+    int virtualPin; // Virtual pin number associated with this controller.
+    int low_limit; // Lower limit value for measurement
+    int high_limit; // Upper limit value for measurement
+    int additive; // additive factor to measurement
+    float factor; // Factor to multiply measurement values by
+
+    //using BasicSensor::sensorName; // previously protected:
+    //using BasicSensor::status;
 };
 
-#endif
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  PinController
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// PinController class that inherits from BasicSensor
+class PinController : public BasicSensor {
+  public:
+      // Total Constructor
+    PinController(const String& name, int pin, int low_limit, int high_limit, int additive, float factor);
+
+    // Short Constructor
+    PinController(const String& name, int pin);
+
+    // JSON Obj Constructor
+    // pass name (key) and config (value)
+    PinController(const String& name, const JsonObject& sensorConfig);
+    
+    // Override the measure function to read from an Arduino pin
+    float measure() override;
+
+  private:
+    int pin;
+    int low_limit; // Lower limit value for measurement
+    int high_limit; // Upper limit value for measurement
+    int additive; // additive factor to measurement
+    float factor; // Factor to multiply measurement values by
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  SoilTempSensor
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// PinController class that inherits from BasicSensor
+class SoilTempSensor : public BasicSensor {
+  public:
+      // Total Constructor
+    SoilTempSensor(const String& name, int addr, int additive, float factor);
+
+    // Short Constructor
+    SoilTempSensor(const String& name, int addr);
+
+    // JSON Obj Constructor
+    // pass name (key) and config (value)
+    SoilTempSensor(const String& name, const JsonObject& sensorConfig);
+    
+    // Override the measure function to read from an Arduino pin
+    float measure() override;
+
+  private:
+    OneWire oneWire;             // OneWire instance
+    DallasTemperature sensors;   // DallasTemperature instance
+    int addr;
+    int additive; // additive factor to measurement
+    float factor; // Factor to multiply measurement values by
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  SoilTempSensor
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This class is used to gahter and publish data from an bme280 using adafruit lib
+class bmeSensor : public BasicSensor {
+  public:
+      // Total Constructor
+    bmeSensor(const String& name, int addr, int mode);
+
+    // JSON Obj Constructor
+    // pass name (key) and config (value)
+    bmeSensor(const String& name, const JsonObject& sensorConfig);
+    
+    // Override the measure function to read from an Arduino pin
+    float measure() override;
+
+  private:
+    Adafruit_BME280 bme; // BME280 sensor instance
+    int addr; // I2C address of the BME280 sensor
+    int mode;
+
+};
+#endif
